e29ab4170ca1dae94711182015327ae48d1d16f8
Tamil-BAE24 <tamil.lenin@wns.com>
2024-08-08 19:48:07 +0530

    Update ProjectSpecificMethod.java

--- ProjectSpecificMethod.java
+++ ProjectSpecificMethod.java
@@ -1,35 +1,74 @@
 package baseclass;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ProjectSpecificMethod {
 	
 	public static RemoteWebDriver driver;
+	public static String reqid;
 	
+	public static ExtentSparkReporter reporter;
+	public static ExtentReports extent;
+	public static ExtentTest test;
+	int i=1;
 	
+	@BeforeTest
+	public void reportstart(ITestContext context)
+	{
+		String testName = context.getCurrentXmlTest().getName();
+		String reportFileName = null;
+
+		if (testName.equals("Regression"))
+		{
+			
+			reportFileName = "./RetailloanBranch.html";	
+			
+		} 
+		
+		reporter = new ExtentSparkReporter(reportFileName);
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+	}
+
 
 	@BeforeMethod
 	public void preCondition() {
 		
+		
+		
 		driver = new ChromeDriver();
 		driver.get("https://bpm-qa-01.baelab.net/suite/tempo/actions");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("un")).sendKeys("LMBranchMaker");
-		driver.findElement(By.id("pw")).sendKeys("1234");
-		driver.findElement(By.xpath("//input[@value='Sign In']")).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.findElement(By.xpath("//span[text()='Retail Loans']")).click();	
+		
+		test = extent.createTest("TC_00" + i);
+		test.assignCategory("Regression");
+		test.assignAuthor("Tamil Selvan Lenin");
+			
+		
 	}
 	
 	@AfterMethod
-	public void postCondition()
-	{
-		//driver.close();
+	public void reportEnd(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.fail(result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			;
+		} else {
+			test.skip("Test skipped");
+		}
+		extent.flush();
+		i++;
+		
+		
 	}
 }
